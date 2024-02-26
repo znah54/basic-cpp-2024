4ab31195d7ba269eeb5a434fb772db11815dd876
znah54 <xcxcxc54@naver.com>
2024-02-27 08:24:57 +0900

    ddd

--- day04/Project1/Project1/a.cpp
+++ day04/Project1/Project1/a.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 class StudentClass {
 private:
-	int ID;
-	char* name;
+	const int ID;  // 학번 상수화
+	char* name;		//문자열 배열을 선언하고 바로 초기화 하지 않는 경우는 strcpy()를 통하여 초기화 한다.
 	char* major;
 	int age;
 public:
-	StudentClass(int id,const char *n, const char *m, int a) {
-		ID = id;
-		strcpy(name, n);
+	// 문자열은 주소기 때문에 포인터 변수에 넣어서 사용한다. 문자열의 첫번째 문자 주소가 저장된다.
+	// 멤버 이니셜랑즈는 객체 생성시 초기화되지않는 멤버들을 초기화할때 사용한다. 즉 객체생성과 상관없이 미리 초기화가 되는지
+	StudentClass(int id,const char *n, const char *m, int a) : ID(id) {
+		//ID = id;
+		strcpy(name, n);			// 문자열배열을 선언하고 바로 초기화하지 않는 경우는 strcpy()를 통하여 초기화 된다.
 		strcpy(major, m);
 		age = a;
 	}
@@ -23,7 +25,7 @@ public:
 };
 int main()
 {
-	StudentClass student(151,"dddd","aaaa",1515);
+	StudentClass student(151,"dddd","aaaa",1515);		//객체를 생성하면 생성자를 호출해야 한다.
 	student.Showmy();
 	return 0;
 }
